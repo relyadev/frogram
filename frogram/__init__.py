12e95c6a1de55c9d7c22d3750e494335f3d9539a
relyadev <relyadev@gmail.com>
2025-06-01 13:02:55 +0300

    version

--- frogram/__init__.py
+++ frogram/__init__.py
@@ -1,3 +1,3 @@
-from .qgram import Bot, inline_button, inline_keyboard, remove_keyboard
+from .frogram import Bot, inline_button, inline_keyboard, remove_keyboard
 
 __all__ = ["Bot", "inline_button", "inline_keyboard","remove_keyboard"]
